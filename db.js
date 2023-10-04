75bb274aeffa3805c504b071249be7578263f788
sumit kumar <sumokhan76342@gmail.com>
2023-10-04 15:53:52 +0530

    changed mongo-url, start script, port

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const mongoURl = "mongodb://localhost:27017/"
 // this Function will connect to the Mongodb
 const ConnectToMongo = async()=>{
-	  await mongoose.connect('mongodb://127.0.0.1:27017/CodingArena');
-	  console.log("moongose connected ")
+	  await mongoose.connect("mongodb+srv://sumit:sumit@cluster0.8dflsuw.mongodb.net/coding_arena")
+	  .then(() => { console.log("MongoDb connected sucessfylly") })
+	  .catch((err) => { console.log(err) })
 }
 
 module.exports = ConnectToMongo; 
